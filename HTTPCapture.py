265cc7e4c98b164fecb97ba09fa3e840d266d805
Nathanv97 <43190392+Nathanv97@users.noreply.github.com>
2019-11-21 15:31:24 -0600

    New version of HTTPCapture.py that adds decoding functionality

--- HTTPCapture.py
+++ HTTPCapture.py
@@ -1,5 +1,5 @@
 import pcapy
-
+from impacket.ImpactDecoder import EthDecoder
 
 #Prints out all devices that we can listen to
 print(pcapy.findalldevs())
@@ -7,6 +7,7 @@ print(pcapy.findalldevs())
 max_bytes = 1024
 promis = False
 timeout = 100
+
 #open a live capture
 pc = pcapy.open_live("eth0", max_bytes, promis, timeout)
 
@@ -14,11 +15,12 @@ pc = pcapy.open_live("eth0", max_bytes, promis, timeout)
 pc.setfilter('port 80')
 
 #print the packets
-def print_packet(hdr, data):
+def print_packet(packet_header, packet_data):
 
-  print data
+  decoded_packet = EthDecoder().decode(packet_data)
+  print decoded_packet
 
 
-#collect 10 packets then stop
+#collect unlimited packets
 packet_limit = -1
 pc.loop(packet_limit, print_packet)
