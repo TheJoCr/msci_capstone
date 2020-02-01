95355c4a769d54b6aed330a012f6e0583e074e49
Jordan Crawford <jordanac95@gmail.com>
2020-02-01 14:56:51 -0600

    Add parsing of TCP and IP headers during capture.

--- cpp/sniffer.cpp
+++ cpp/sniffer.cpp
@@ -1,14 +1,81 @@
 #include <stdio.h>
 #include <pcap.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/if_ether.h> /* to parse Ethernet headers. */
+#include <netinet/ip.h> /* to parse IP headers. */
+#include <netinet/tcp.h> /* to parse TCP headers. */
 
-static int pktCount = 0;
 
 // do somthing with the packet
 void getHTTPRequest(u_char *data, const struct pcap_pkthdr* header, const u_char* packet)
 {
-	//print out when a packet is received for now.
-	++pktCount;	
-	printf("packet captured of length: %d\n", header->len);
+	struct ether_header *ether;
+	struct ip *ip;
+	struct tcphdr *tcp;
+
+	/////////
+	// ETH //
+	/////////
+	
+	// Parse Ethernet header, skip those bytes.
+	ether = (struct ether_header*) packet;
+	int ether_header_len = sizeof(struct ether_header); // constant size
+	packet += ether_header_len;
+
+	/////////
+	// IP  //
+	/////////
+	
+	// Parse IP Header, skip those bytes
+	ip = (struct ip*) packet;
+	int ip_header_len = ip->ip_hl * 4; 
+	packet += ip_header_len;
+	
+	// Get some of the IP info
+	in_addr source_ip = ip->ip_src;
+	in_addr dest_ip = ip->ip_dst;
+
+	// Convert that to strings
+	char source_ip_str[INET_ADDRSTRLEN];
+	char dest_ip_str[INET_ADDRSTRLEN];
+	inet_ntop(AF_INET, &(source_ip), source_ip_str, INET_ADDRSTRLEN);
+	inet_ntop(AF_INET, &(dest_ip),   dest_ip_str,   INET_ADDRSTRLEN);
+
+	/////////
+	// TCP //
+	/////////
+	
+	// Parse TCP header, skip those bytes
+	tcp = (struct tcphdr*) packet;
+	int tcp_data_offset = tcp->doff * 4; 
+	packet += tcp_data_offset;
+
+	// Get a bit of TCP info. Needs to be converted from network endianness
+	// to local.
+	uint16_t source_port = ntohs( tcp->th_sport );
+	uint16_t dest_port = ntohs( tcp->th_dport );
+	uint32_t seq_num = ntohs( tcp->th_seq );
+	uint32_t ack_num = ntohs( tcp->th_ack );
+
+	/////////
+	// OUT //
+	/////////
+
+	printf("Packet from %s:%d to %s:%d (seq: %d, ack: %d)\n", 
+			source_ip_str, source_port,
+			dest_ip_str, dest_port,
+			seq_num, ack_num);
+
+	int body_size = header->caplen - ether_header_len - ip_header_len - tcp_data_offset;
+
+	if (body_size > 0) {
+		printf("Body (%d bytes):\n", body_size);
+		for( int i = 0; i < body_size; i++ ) {
+			printf("%c", packet[i] );
+		}
+	}
 }
 
 
@@ -20,13 +87,14 @@ int main(int argc, char *argv[])
 	char errbuf[PCAP_ERRBUF_SIZE];	// error string
 	struct pcap_pkthdr header;	// the packet header
 	const u_char *packet;		// the packet
-	char filter[] = "port 80";      // the filter
+	char filter[] = "port 8000";    // the filter
 	struct bpf_program fp;		// compiled filter expression
 	bpf_u_int32 mask;		// the netmask of the sniffing device
 	bpf_u_int32 net;		// the ip of sniffing device
 
 	// assign the device
-	dev = pcap_lookupdev(errbuf);
+	// dev = pcap_lookupdev(errbuf);
+	dev = (char*) "lo";
 	if (dev == NULL) {
 		fprintf(stderr, "Couldn't find default device: %s\n", errbuf);
 		return(2);
